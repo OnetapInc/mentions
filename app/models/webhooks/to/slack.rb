dc07ccd633750b498b0158f597c24f6b963b1b27
Yoshiki Takeda <goleador0322@YoshikinoMacBook-puro.local>
2020-06-19 21:29:45 +0900

    fuck

--- app/models/webhooks/to/slack.rb
+++ app/models/webhooks/to/slack.rb
@@ -1,12 +1,11 @@
 class Webhooks::To::Slack
   def initialize(mention:, url:, additional_message:)
     @mention = "@#{mention}"
-    @channel = @mention == '@everyone' ? '#general' : @mention
     @text = "#{@mention} #{url} #{additional_message}"
     @webhook_uri = ENV.fetch('SLACK_WEBHOOK_URL')
   end
 
   def post
-    HttpPostJob.perform_later(@webhook_uri, { payload: {text: @text, link_names: 1, channel: @channel}.to_json })
+    HttpPostJob.perform_later(@webhook_uri, { payload: {text: @text, link_names: 1, channel: "#mention-of-esa-and-gh"}.to_json })
   end
 end
